2ee1e177580518edc1235048b78b6ff954a5664e
Ian Andrusiak <Ian@macbook-pro.gateway.2wire.net>
2016-02-24 22:02:24 -0500

    Added course codes endpoint

--- course_info.php
+++ course_info.php
@@ -13,9 +13,11 @@
       //Year selection - Finds all available terms
       $connection = mysqli_connect($host, $user, $password, $dbname) or die("Error " . mysqli_connect_error());
 
+      //Queries all semesters in db
       $sql = "SELECT year, term FROM Semesters ORDER BY year DESC, term DESC";
       $result = $connection->query($sql);
 
+      //Prepares results to be converted to JSON
       $semesters = array();
       if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
@@ -27,9 +29,34 @@
       mysqli_close($connection);
     } elseif($query_type == 'course_code') {
       //Course codes - Finds all course codes for specified semester
+      $connection = mysqli_connect($host, $user, $password, $dbname) or die("Error " . mysqli_connect_error());
 
+      //Ensures appropriate parameters specified
+      if(!isset($_GET['subject']) || !isset($_GET['code']) || !isset($_GET['year']) || !isset($_GET['term'])) {
+        throw new Exception('Incorrect Parameters Specified');
+      }
+      $subj = $_GET['subject']);
+      $code = $_GET['code']);
+      $year = $_GET['year']);
+      $term = $_GET['term']);
 
+      //Queries for course codes that match specified semester
+      $course_sql = $connection->prepare('SELECT subject, code FROM Courses '
+        .'LEFT JOIN Semesters ON semester_id = Semesters.id '
+        .'WHERE subject = ? AND code = ? AND year = ? AND term = ?');
+      $course_sql->bind_param('ssss', $subj, $code, $year, $term);
+      $course_sql->execute();
 
+      //Prepares results to be converted to JSON
+      $course_codes = array();
+      $course_sql->bind_result($course_subj, $course_code);
+      while ($course_sql->fetch()) {
+        array_push($course_codes, array($course_subj.' '.$course_code));
+      }
+
+      echo json_encode($course_codes);
+      $course_sql->close();
+      mysqli_close($connection);
     } elseif($query_type == 'course_info') {
       //Course selection - Finds section/session info for specified course
 
