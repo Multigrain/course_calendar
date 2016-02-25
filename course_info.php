29948bd96d97a3161d80819d9038311802402715
Ian Andrusiak <Ian@macbook-pro.gateway.2wire.net>
2016-02-25 00:30:10 -0500

    Added limit to autofill

--- course_info.php
+++ course_info.php
@@ -41,7 +41,7 @@
         $connection = mysqli_connect($host, $user, $password, $dbname) or die("Error " . mysqli_connect_error());
         $course_sql = $connection->prepare("SELECT subject, code FROM Courses ".
           "LEFT JOIN Semesters ON semester_id = Semesters.id WHERE year = ? AND ".
-          "term = ? AND concat_ws(' ', subject, code) like ?");
+          "term = ? AND concat_ws(' ', subject, code) like ? ORDER BY subject ASC, code ASC LIMIT 5");
         $course_sql->bind_param('sss', $year, $term, $key_word);
         $course_sql->execute();
 
