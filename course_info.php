c30e6a216b43fc70c13819e74b8698fa4d6aa163
Ian Andrusiak <Ian@Ians-MacBook-Pro.local>
2016-02-25 15:08:01 -0500

    debugging ci endpoint

--- course_info.php
+++ course_info.php
@@ -67,13 +67,11 @@
         $term = $_GET['term'];
         $subject = $_GET['subject'];
         $code = $_GET['code'];
+        /*
+        $connection = mysqli_connect($host, $user, $password, $dbname) or die("Error " . mysqli_connect_error());
+        echo getCourseID($connection, $subject, $code, $year, $term);
+        $course_sql->close();*/
       }
-
-      $connection = mysqli_connect($host, $user, $password, $dbname) or die("Error " . mysqli_connect_error());
-      echo getCourseID($connection, $subject, $code, $year, $term);
     }
-
   }
-
-
 ?>
