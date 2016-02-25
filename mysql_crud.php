bca48d78677701fa8f1a40b8dbd2b2b5b9f758b6
Ian Andrusiak <Ian@Ians-MacBook-Pro.local>
2016-02-25 15:14:37 -0500

    debugging ci endpoint

--- mysql_crud.php
+++ mysql_crud.php
@@ -3,14 +3,13 @@
     //Finds course id that matches specified parameters
     $course_sql = $connection->prepare("SELECT Courses.id FROM Courses ".
       "LEFT JOIN Semesters ON semester_id = Semesters.id ".
-      "WHERE subject = ? AND code = ? AND year = ? AND term = ?";
+      "WHERE subject = ? AND code = ? AND year = ? AND term = ?");
     $course_sql->bind_param('ssss', $subject, $code, $year, $term);
     $course_sql->execute();
 
     //Reads results
-    $course_sql->bind_result($id);
+    $course_sql->bind_result($course_id);
     $course_sql->fetch();
-    $course_id = $id;
     $course_sql->close();
 
     return $course_id;
